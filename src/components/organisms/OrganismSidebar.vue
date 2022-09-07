5f3d12560d720a4061ebe171ccb2ff95f6917223
Sergey Ryabovalov <sergey.ryabovalov@bravedevelopers.com>
2022-09-07 05:18:56 -0700

    feat: add basic range slider component

--- src/components/organisms/OrganismSidebar.vue
+++ src/components/organisms/OrganismSidebar.vue
@@ -15,21 +15,25 @@
     <section class="sidebar__section">
       <h3 class="sidebar__title">Price range</h3>
 
-      <AtomInputRange />
+      <Slider v-model="value" class="range__slider" />
     </section>
   </aside>
 </template>
 
 <script lang="ts" setup>
-import AtomInputRange from "@/components/atoms/AtomInputRange.vue";
 import MoleculeBrandBlock from "@/components/molecules/MoleculeBrandBlock.vue";
+import Slider from "@vueform/slider";
 import { ref } from "vue";
+
+const value = [0, 20];
 
 const items = ref([
   { id: 1, title: "someone" },
   { id: 2, title: "222" },
 ]);
 </script>
+
+<style lang="scss" src="@vueform/slider/themes/default.scss"></style>
 
 <style lang="scss" scoped>
 @import "@/styles/variables.scss";
@@ -58,5 +62,13 @@ const items = ref([
   flex-direction: column;
   gap: 14px;
   margin-top: 16px;
+}
+
+.range__slider {
+  --slider-bg: rgba(0, 0, 0, 0.15);
+  --slider-height: 2px;
+  --slider-connect-bg: #000;
+  --slider-tooltip-bg: #000;
+  --slider-handle-ring-color: rgba(0, 0, 0, 0.15);
 }
 </style>
