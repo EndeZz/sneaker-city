80dc5cf79489b887d34e86a7877f6263c70a08c4
Sergey Ryabovalov <sergey.ryabovalov@bravedevelopers.com>
2022-09-09 07:06:28 -0700

    refactor: move api url to constant && update apiRequests file

---
+++ src/constants/api.constant.ts
@@ -0,0 +1,1 @@
+export const API_CONFIG = "https://fakestoreapi.com/products";
--- src/utils/api/apiRequests.ts
+++ src/utils/api/apiRequests.ts
@@ -1,43 +1,40 @@
+import { API_CONFIG } from "@/constants/api.constant";
 import { IProduct } from "@/models/product.interfaces";
 import axios from "axios";
 
 export const fetchProductsByCategory = async (category: string) => {
   try {
     const currentUrl = category
-      ? `https://fakestoreapi.com/products/category/${category}`
-      : `https://fakestoreapi.com/products`;
+      ? `${API_CONFIG}/category/${category}`
+      : `${API_CONFIG}`;
 
     const respValues = await axios.get<IProduct[]>(currentUrl);
     const dataValues = respValues.data;
 
     return dataValues;
   } catch (e) {
-    throw new Error("Failed to connect API");
+    throw new Error("Failed to connect to API");
   }
 };
 
 export const fetchCategories = async () => {
   try {
-    const respValues = await axios.get(
-      "https://fakestoreapi.com/products/categories"
-    );
+    const respValues = await axios.get<string[]>(`${API_CONFIG}/categories`);
     const dataVales = respValues.data;
 
     return dataVales;
   } catch (e) {
-    throw new Error("Failed to connect API");
+    throw new Error("Failed to connect to API");
   }
 };
 
 export const fetchProduct = async (productId: number) => {
   try {
-    const respValues = await axios.get(
-      `https://fakestoreapi.com/products/${productId}`
-    );
+    const respValues = await axios.get<IProduct>(`${API_CONFIG}/${productId}`);
     const dataVales = respValues.data;
 
     return dataVales;
   } catch (e) {
-    throw new Error("Failed to connect API");
+    throw new Error("Failed to connect to API");
   }
 };
